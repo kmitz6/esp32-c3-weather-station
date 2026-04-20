8b706187bbafb5f97cf3bd552448eaa0ecf3aba4
kmitz6 <123777279+kmitz6@users.noreply.github.com>
2026-04-20 13:12:54 +0200

    Update config.h

--- config.h
+++ config.h
@@ -18,7 +18,7 @@ const char* path     = "/api/sensors";
 constexpr float TEMP_CALIB   = -4.0;
 constexpr float PRESS_CALIB  = 15.0;
 constexpr float HUM_CALIB    = 0.0;
-constexpr float WIND_CALIB   = 1.0;
+constexpr float WIND_CALIB   = 1.1;
 constexpr float PM25_CALIB   = 1.0;
 constexpr float PM10_CALIB   = 1.0;
 
@@ -33,13 +33,24 @@ constexpr int IR_PIN         = 8;
 constexpr unsigned long SENSOR_READ_INTERVAL = 3000;
 constexpr unsigned long SEND_INTERVAL        = 30000;
 constexpr unsigned long WIFI_RETRY_INTERVAL  = 10000;
-constexpr unsigned long DEBOUNCE_US          = 20000;
+constexpr unsigned long DEBOUNCE_US          = 15000;
 
 // Buffer
 constexpr int BUFFER_SIZE = 15;
+int bufferIndex = 0;
+int bufferCount = 0;
 
+// sensors
 extern HardwareSerial pms;
 extern Adafruit_BMP280 bmp;
 extern Adafruit_AHTX0 aht;
+
+bool bmp_ok = false;
+bool aht_ok = false;
+
+volatile unsigned long irPulseCount = 0;
+volatile unsigned long lastIrTime = 0;
+float windKmh = 0.0;
+unsigned long lastWindCalc = 0;
 
 #endif
